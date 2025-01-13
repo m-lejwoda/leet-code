b1a80464fddf820edb82a477c390b3930a96d86f
michal <michal.lejwoda@gmail.com>
2025-01-13 21:36:37 +0100

    739.

---
+++ algomap/Stacks/739. Daily Temperatures.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        res = [0] * n
+        stack = []
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                res[prev_index] = i - prev_index
+            stack.append(i)
+
+        return res
+
+s = Solution()
+print(s.dailyTemperatures([73,74,75,71,69,72,76,73]))
+print(s.dailyTemperatures([30,40,50,60]))
+print(s.dailyTemperatures([30,60,90]))
