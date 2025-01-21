5be6a48cf5c22e8d16187cd445e4c244e1c1ebae
michal <michal.lejwoda@gmail.com>
2025-01-21 23:41:52 +0100

    367. v2

--- algomap/binary_search/367. Valid Perfect Square.py
+++ algomap/binary_search/367. Valid Perfect Square.py
@@ -1,12 +1,32 @@
 from math import sqrt
 
+# Try Binary
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        pass
+        # if int(sqrt(num)) == sqrt(num):
+        #     return True
+        # return False
+
+
+# class Solution:
+#     def isPerfectSquare(self, num: int) -> bool:
+#         if int(sqrt(num)) == sqrt(num):
+#             return True
+#         return False
 
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        if int(sqrt(num)) == sqrt(num):
-            return True
+        l, r = 0, num
+        while l <= r:
+            middle = (l + r) // 2
+            if (middle * middle) == num:
+                return True
+            elif (middle * middle) < num:
+                l = middle + 1
+            else:
+                r = middle - 1
         return False
-
 
 s = Solution()
 s.isPerfectSquare(100)
