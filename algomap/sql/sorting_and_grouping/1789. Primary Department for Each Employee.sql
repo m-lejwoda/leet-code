70d1c1954e617f6ac28302d045b96d59afcb30b6
michal <michal.lejwoda@gmail.com>
2025-02-18 13:13:30 +0100

    1789.

---
+++ algomap/sql/sorting_and_grouping/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,7 @@
+
+Select employee_id, department_id
+from Employee
+where primary_flag = 'Y'
+    or employee_id in (Select employee_id from Employee
+                        group by employee_id
+                        having count(department_id) = 1)
